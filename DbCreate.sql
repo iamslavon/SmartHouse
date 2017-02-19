b94032b3ef8930c935766a35446274fa6a6ffffd
SlavBro <iamslavon@gmail.com>
2017-02-19 16:48:41 +0200

    Changed DbCreate script

--- DbCreate.sql
+++ DbCreate.sql
@@ -14,4 +14,20 @@ CREATE TABLE [SensorData](
 [Time] [datetime2] NOT NULL)
 GO
 
+CREATE TABLE [Rooms]( 
+[Id] [int] PRIMARY KEY IDENTITY NOT NULL, 
+[HouseId] [int] NOT NULL, 
+[Name] [nvarchar](50))
+GO
+
+CREATE TABLE [Houses]( 
+[Id] [int] PRIMARY KEY IDENTITY NOT NULL, 
+[Name] [nvarchar](50))
+GO
+
+CREATE TABLE [Sensors]( 
+[Id] [int] PRIMARY KEY IDENTITY NOT NULL, 
+[Name] [nvarchar](50))
+GO
+
 --INSERT INTO [SensorData] ([HouseId], [RoomId], [SensorId], [Value]) OUTPUT Inserted.ID VALUES (1, 1, 1, 100);
