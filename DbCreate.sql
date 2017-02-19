91b2559ded7e29628b9cce93e2cb13d2c94f77d1
SlavBro <iamslavon@gmail.com>
2017-02-19 16:55:56 +0200

    Added relations to tables

--- DbCreate.sql
+++ DbCreate.sql
@@ -5,21 +5,6 @@ GO
 USE [SmartHouse]
 GO 
 
-CREATE TABLE [SensorData]( 
-[Id] [int] PRIMARY KEY IDENTITY NOT NULL, 
-[HouseId] [int] NOT NULL, 
-[RoomId] [int] NOT NULL, 
-[SensorId] [int] NOT NULL, 
-[Value] [int],
-[Time] [datetime2] NOT NULL)
-GO
-
-CREATE TABLE [Rooms]( 
-[Id] [int] PRIMARY KEY IDENTITY NOT NULL, 
-[HouseId] [int] NOT NULL, 
-[Name] [nvarchar](50))
-GO
-
 CREATE TABLE [Houses]( 
 [Id] [int] PRIMARY KEY IDENTITY NOT NULL, 
 [Name] [nvarchar](50))
@@ -28,6 +13,21 @@ GO
 CREATE TABLE [Sensors]( 
 [Id] [int] PRIMARY KEY IDENTITY NOT NULL, 
 [Name] [nvarchar](50))
+GO
+
+CREATE TABLE [Rooms]( 
+[Id] [int] PRIMARY KEY IDENTITY NOT NULL, 
+[HouseId] [int] FOREIGN KEY REFERENCES [Houses]([Id]),
+[Name] [nvarchar](50))
+GO
+
+CREATE TABLE [SensorData]( 
+[Id] [int] PRIMARY KEY IDENTITY NOT NULL, 
+[HouseId] [int] FOREIGN KEY REFERENCES [Houses]([Id]), 
+[RoomId] [int] FOREIGN KEY REFERENCES [Rooms]([Id]), 
+[SensorId] [int] FOREIGN KEY REFERENCES [Sensors]([Id]),
+[Value] [int],
+[Time] [datetime2] NOT NULL)
 GO
 
 --INSERT INTO [SensorData] ([HouseId], [RoomId], [SensorId], [Value]) OUTPUT Inserted.ID VALUES (1, 1, 1, 100);
