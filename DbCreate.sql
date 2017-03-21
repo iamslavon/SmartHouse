4719f0dc3dec9924706b83b318bba45a62cf24f0
SlavBro <iamslavon@gmail.com>
2017-03-22 00:56:05 +0200

    Added stored procedures to DbCreate script

--- DbCreate.sql
+++ DbCreate.sql
@@ -32,3 +32,65 @@ CREATE TABLE [SensorData](
 GO
 
 --INSERT INTO [SensorData] ([HouseId], [RoomId], [SensorId], [Value]) OUTPUT Inserted.ID VALUES (1, 1, 1, 100);
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[GetHouses]
+AS
+BEGIN
+	SET NOCOUNT ON;
+	SELECT		[Houses].[Id],
+				[Houses].[Name]
+	FROM		[Houses]
+END
+GO
+
+CREATE PROCEDURE [dbo].[GetLastSensorValue]
+	@HouseId int, 
+	@RoomId int,
+	@SensorId int
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT TOP 1	[sd].[Value], [sd].[Time]
+	FROM			[SensorData] [sd]
+	WHERE			[sd].[HouseId] = @HouseId AND [sd].[RoomId] = @RoomId AND [sd].[SensorId] = @SensorId
+	ORDER BY		[sd].[Time] DESC
+END
+GO
+
+CREATE PROCEDURE [dbo].[GetRoomsForHouse]
+	@HouseId int
+AS
+BEGIN
+	SET NOCOUNT ON;
+	SELECT	[Rooms].[Id],
+			[Rooms].[Name]
+	FROM	[Rooms]
+	WHERE	[Rooms].[HouseId] = @HouseId
+END
+GO
+
+CREATE PROCEDURE [dbo].[GetSensorsForRoom]
+	@HouseId int, 
+	@RoomId int
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+    SELECT [s].[Id], [s].[Name], [s].[Measurement]
+	FROM
+		(SELECT [sd].[SensorId]
+		FROM [SensorData] [sd]
+		WHERE	[sd].[HouseId] = @HouseId 
+			AND [sd].[RoomId] = @RoomId
+		GROUP BY [sd].[SensorId]) tt
+	INNER JOIN [Sensors] [s]
+	ON [s].[Id] = [tt].[SensorId]
+END
+GO
